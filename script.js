edf3f71715ae6d52f1a077e4c27a8508278100a5
AmjustGettingStarted <gamerhmv2050@gmail.com>
2023-04-11 21:06:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// num=document.getElementById("number")
+
+function incriment(){
+
+    var number = Number( document.getElementById('number').innerHTML);
+    document.getElementById('number').innerHTML= number+1;
+
+}
+
+function decrement(){
+    
+    var number = Number( document.getElementById('number').innerHTML);
+    document.getElementById('number').innerHTML= number-1;
+
+}
+
+function neutral() {
+    
+    var number = Number( document.getElementById('number').innerHTML);
+    document.getElementById('number').innerHTML= number-number;
+    // document.getElementById('number').innerHTML= 0;
+    
+}
